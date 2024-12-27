7801c5c7d6211b79f8fd468b22b6d7aae295d371
Santhiya natarajan <santhiyakn01@gmail.com>
2024-12-27 16:14:04 +0530

    Refactor MongoDB connection to use environment variable

--- models/db.js
+++ models/db.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 import createAdmin from '../utitlity/createAdmin.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 export const connectDb =async()=>{
-    // const ServiceProvider = mongoose.model('ServiceProvider', );
-await mongoose.connect('mongodb://127.0.0.1:27017/lssems-DataBase').then(async() => {
+    
+await mongoose.connect(process.env.MONGO_URI).then(async() => {
     console.log('Connected to database');
-    // await ServiceProvider.collection.dropIndex('phoneNumber_1');
     await createAdmin();
 
     }).catch((error) => {
